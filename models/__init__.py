b73bcd0eeb9c685f793abe14710f6b15bb74e0e1
valeriylo <valery2698@mail.ru>
2024-07-09 17:14:57 +0300

    redundant model names removed

--- models/__init__.py
+++ models/__init__.py
@@ -5,13 +5,11 @@ from models.hu1dcnn_kan import Hu1DCNN_KAN
 from models.hu1dcnn_full_kan import Hu1DCNN_KAN as Hu1DCNN_FULL_KAN
 from models.m1dcnn import M1DCNN
 from models.m1dcnn_kan import M1DCNN_KAN
-from models.m1dcnn_full_kan import M1DCNN_KAN as M1DCNN_FULL_KAN
 from models.luo3dcnn import Luo3DCNN
 from models.luo3dcnn_kan import Luo3DCNN_KAN
 from models.he3dcnn import He3DCNN
 from models.he3dcnn_kan import He3DCNN_KAN
 from models.nm3dcnn import NM3DCNN
 from models.nm3dcnn_kan import NM3DCNN_KAN
-from models.nm3dcnn_full_kan import NM3DCNN_KAN as NM3DCNN_FULL_KAN
 from models.ssftt import SSFTT
 from models.ssftt_kan import SSFTT_KAN
